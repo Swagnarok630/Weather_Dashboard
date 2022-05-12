bf64906fe7d89078ea1f2935df424ef97db28dca
Swagnarok <darryl.le.630@gmail.com>
2022-05-12 02:40:02 -0700

    Add fetches for location and weather data

--- assets/script.js
+++ assets/script.js
@@ -15,5 +15,36 @@ function cityinput(event) {
 
     // Prevent default needed to stop input from actually being submitted by form element and retain input value for variable
     event.preventDefault();
+    convert(input)
     console.log(input)
+}
+
+// Function to take user's city input and convert to raw data to grab latitude and longitude
+function convert() {
+    var geocode = `${weatherAPIbaseURL}/geo/1.0/direct?q=${input}&limit=5&appid=${weatherAPIkey}`;
+
+    fetch(geocode)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (geodata) {
+            console.log(geodata)
+            weather(geodata)
+        })
+}
+
+//
+function weather(geodata) {
+    var lat = geodata[0].lat;
+    var lon = geodata[0].lon;
+    var city = geodata[0].name;
+    var weather = `${weatherAPIbaseURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${weatherAPIkey}`;
+
+    fetch(weather)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (weatherdata) {
+            console.log(weatherdata)
+        })
 }
