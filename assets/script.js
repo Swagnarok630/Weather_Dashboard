55e5d92328d08e79961528440e8f3f76da705abe
Swagnarok <darryl.le.630@gmail.com>
2022-05-14 05:19:59 -0700

    Finalized majority of logic. Need to add catches for errors and invalid data. Focus on stylization next and possible extra features

--- assets/script.js
+++ assets/script.js
@@ -5,25 +5,102 @@ var weatherAPIkey = "94ebea31571843c6c5e0f41fe628959f";
 // Global variables
 var search = document.querySelector("#search");
 var userinput = document.querySelector("#city");
-var stuff = document.querySelector("#stuff");
 var main = document.querySelector(".maincard") ;
-var forecast = document.querySelector(".forecast")
+var forecast = document.querySelector(".forecast");
+var pastcontainer = document.querySelector(".history");
+var searchhistory = [];
 
-// Search button
-search.addEventListener('submit', cityinput);
+// Run to initialize any history buttons for local storage
+historyinitial()
+
+// Search button and history buttons
+search.addEventListener("submit", cityinput);
+pastcontainer.addEventListener("click", historyclick);
 
 // Function to grab user's input
 function cityinput(event) {
+    // Prevents search if form is empty
+    if (!userinput.value) {
+        return;
+    }
+    // Eliminates spaces around user input and sets value
     input = userinput.value.trim();
 
     // Prevent default needed to stop input from actually being submitted by form element and retain input value for variable
     event.preventDefault();
-    convert(input)
-    console.log(input)
+    // Take user input and run into API fetch
+    convert(input);
+    // Empty out form once search is conducted
+    userinput.value = "";
+    // console.log(input);
 }
 
+// Function to add inputs to history
+function historylog(input) {
+    // If the search item is already in the history list, we don't add to the list
+    if (searchhistory.indexOf(input) !== -1) {
+        return;
+    }
+    // If array has 10 items already, remove oldest entry and replace with new entry
+    if (searchhistory.length === 10) {
+        searchhistory.splice(0,1);
+        searchhistory.push(input);
+    }
+    else {
+    // Adding input to array
+    searchhistory.push(input);
+    }
+    // console.log(searchhistory);
+    // Setting to local storage
+    localStorage.setItem("searchhistory", JSON.stringify(searchhistory));
+    // Take input and create button
+    historybuttons()
+}
+
+// Function to add search history as buttons to a search history list
+function historybuttons() {
+    // Clear section and re-initialize with search history
+    pastcontainer.innerHTML = "";
+
+    for (var i = searchhistory.length - 1; i >= 0; i--) {
+        var button = document.createElement("button");
+        button.setAttribute("type", "button");
+        button.textContent = searchhistory[i];
+        // To allow access to city name for historyclick function
+        button.setAttribute("searched", searchhistory[i])
+        // To help determine if historyclick will need to perform search or not
+        button.classList.add("buttonhistory")
+        pastcontainer.append(button);
+    }
+}
+
+// Function to initialize the history list
+function historyinitial() {
+    // Grab list from local storage
+    var storedhistory = localStorage.getItem("searchhistory");
+    if (storedhistory) {
+        searchhistory = JSON.parse(storedhistory);
+    }
+    // Parse list into buttons for the history list
+    historybuttons();
+}
+
+// Function to allow history list to be clicked and perform searches
+function historyclick(event) {
+    // If item doesn't have buttonhistory class (any item not a button) it won't attempt a search
+    if (!event.target.matches(".buttonhistory")) {
+        return;
+    }
+    // Button clicked will have value of attribute
+    var button = event.target;
+    var input = button.getAttribute("searched");
+    // Uses attribute as input and performs fetch through API
+    convert(input);
+}
+
+
 // Function to take user's city input and convert to raw data to grab latitude and longitude
-function convert() {
+function convert(input) {
     var geocode = `${weatherAPIbaseURL}/geo/1.0/direct?q=${input}&limit=5&appid=${weatherAPIkey}`;
 
     fetch(geocode)
@@ -31,8 +108,10 @@ function convert() {
             return response.json();
         })
         .then(function (geodata) {
-            console.log(geodata)
-            weather(geodata)
+            // console.log(geodata);
+            // Push to history list and perform next API search
+            historylog(input);
+            weather(geodata);
         })
 }
 
@@ -48,10 +127,11 @@ function weather(geodata) {
             return response.json();
         })
         .then(function (weatherdata) {
-            console.log(weatherdata)
-            console.log(city)
-            currentcard(city, weatherdata)
-            futurecards(city, weatherdata)
+            // console.log(weatherdata);
+            // console.log(city);
+            // Take data from fetch and create weather cards for current day and forecast
+            currentcard(city, weatherdata);
+            futurecards(city, weatherdata);
         })
 }
 
@@ -102,14 +182,14 @@ function currentcard(city, weatherdata) {
 }
 
 function futurecards(city, weatherdata) {
-    // Needs to be outside of loop to clear 5-day forecast or it'd delete itself
+    // Needs to be outside of loop to clear 5-day forecast or it'd delete itself per day
     forecast.innerHTML = "";
     // Loop to grab weather data for the next 5 days and attach to card
     for (var i = 1; i < 6; i++) {
         var date = moment().add(i, "days").format("L");
         var icon = "http://openweathermap.org/img/wn/" + weatherdata.daily[i].weather[0].icon + ".png";
-        var icondes = weatherdata.daily[i].weather[0].description
-        var condition = weatherdata.daily[i].weather[0].main
+        var icondes = weatherdata.daily[i].weather[0].description;
+        var condition = weatherdata.daily[i].weather[0].main;
         var temp = weatherdata.daily[i].temp.day;
         var humid = weatherdata.daily[i].humidity;
         var wind = weatherdata.daily[i].wind_speed;
